442288af6de0056969169e1f9b803bfb805c4702
sonagiing <sonagiing@naver.com>
2024-02-01 18:06:39 +0900

    feat.test

---
+++ src/main/java/com/testYM/TestYmApplication.java
@@ -0,0 +1,13 @@
+package com.testYM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestYmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestYmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testYM/TestYmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testYM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestYmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
